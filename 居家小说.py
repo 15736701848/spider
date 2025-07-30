543db5cb0a04dfb92ccedfdb83cb22cd707c5019
panghehe666 <15736701848@163.com>
2025-07-30 18:54:34 +0800

    777

--- 居家小说.py
+++ 居家小说.py
@@ -1,20 +1,22 @@
-import requests                                         
+import requests                                  
 from bs4 import BeautifulSoup
 import time
 
-headers{
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
-        }
 
-def get(url):
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+}
+
+def get(url): 
     try:
         r = requests.get(url, headers=headers)
-        soup = BeautifulSoup(r.text,'html.parser')
+        soup = BeautifulSoup(r.text,'html.parser')              
         content = soup.find('div', id='chaptercontent')
         title = soup.find('span', class_='title').get_text()
         text = content.get_text(separator='\n', strip=True)
-        if len(text)>60:                                            
+        if len(text)>60:
             print(f'正在下载{title}...')
             with open(f'{title}.txt','w')as t:
                 t.write(text)
@@ -23,9 +25,11 @@ def get(url):
         print(f'获取失败{e}')
 
 
-for i in range(200,501):
-    url = f'https://m.a9eef.icu/index/21863/{i}.html'
+for i in range(1,501):
+    url = f'https://fe68c1592abb7b99132c24.577ff.cfd/book/40684/{i}.html'
     get(url)
     for j in range(2,5):
-        url = f'https://m.a9eef.icu/index/21863/{i}_{j}.html'
+        url = f'https://fe68c1592abb7b99132c24.577ff.cfd/book/40684/{i}_{j}.html'
         get(url)
+
+
