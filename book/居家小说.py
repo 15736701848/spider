f3eaf62cda7ef75fdc4634d1e44027bd1c32bec0
panghehe666 <15736701848@163.com>
2025-08-01 07:34:26 +0800

    简洁

--- book/居家小说.py
+++ book/居家小说.py
@@ -29,8 +29,9 @@ def spider(url):
         else:
             logging.info(f'正在下载{title1}...')
             text = text1
-            with open(f'{title1}.txt', 'w') as t:
-                t.write(text1)
+            with open(f'{title1}.txt', 'w', encoding='utf-8') as f:
+                f.write(text1)
+
             time.sleep(1)
 
     except Exception as e:
@@ -38,28 +39,13 @@ def spider(url):
 
 
 
-def get(url):
-    try:
-        r = requests.get(url, headers=headers)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        content = soup.find('div', id='chaptercontent')
-        title = soup.find('span', class_='title').get_text()
-        global text
-        text = content.get_text(separator='\n', strip=True)
-        logging.info(f'正在获取{title}')
-        with open(f'{title}.txt', 'w') as t:
-            t.write(text)
-        time.sleep(1)
-    except Exception as e:
-        logging.info(f'获取失败{e}')
-
 
 os.makedirs("斗罗大陆", exist_ok=True)
 os.chdir("斗罗大陆")
 
 for i in range(1, 501):
     url = f'https://fe68c1592abb7b99132c24.577ff.cfd/book/40684/{i}.html'
-    get(url)
+    spider(url)
     for j in range(2, 5):
         url = f'https://fe68c1592abb7b99132c24.577ff.cfd/book/40684/{i}_{j}.html'  
         statue = spider(url)	
